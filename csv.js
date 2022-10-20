033cdb7af93f7e79b9f89e8be9b833593596b1be
Lucas-Lalarcas <lucasbarbosa0217@gmail.com>
2022-10-20 18:25:28 -0300

    Várias coisas nem sei dizer

--- csv.js
+++ csv.js
@@ -2,15 +2,19 @@
  console.log(container)
     id = 1;
       id = document.URL.split("=")
+      console.log(id)
+      if(id.length > 1){
       id = parseInt(id[1]);
       console.log(id)
       fetch(`content.json`)
+  
       .then((data) => data.json())
       .then((data)=>{
-           
                 container.innerHTML = data.find(x => x.id === id).content.replaceAll("$", `"`)
+      }).catch((error) => {container.innerHTML = `<h1>Erro 404</h1> <p>${error}<p>`})
     
-      })
+    }
+    
 
 
 
--- darkmode.js
+++ darkmode.js
@@ -1,4 +1,4 @@
-container = document.getElementById("main");
+container = document.getElementsByClassName("container");
 toggle = document.getElementById("darktoggle");
 menutoggle = document.getElementById("menutoggle")
 menu = document.getElementById("menu")
@@ -7,12 +7,18 @@ nav = document.getElementsByClassName("nav")
 toggle.addEventListener('input', function(){
     if (toggle.checked)
     {
-        container.style.backgroundColor = "#101010";
-        container.style.color="white";
+        for(i = 0; container.length; i++){
+            container[i].style.backgroundColor = "#101010";
+            container[i].style.color="white";
+        }
+       
     }
     else{
-        container.style.backgroundColor = "#FFFFFF";
-        container.style.color="black";
+
+        for(i = 0; container.length; i++){
+        container[i].style.backgroundColor = "#FFFFFF";
+        container[i].style.color="black";}
+
     }
 })
 
