4ef781a3fe73bd106eafc25346043bd087f87379
Lucas-Lalarcas <lucasbarbosa0217@gmail.com>
2022-10-19 23:58:30 -0300

    Index

--- darkmode.js
+++ darkmode.js
@@ -2,6 +2,8 @@ container = document.getElementById("main");
 toggle = document.getElementById("darktoggle");
 menutoggle = document.getElementById("menutoggle")
 menu = document.getElementById("menu")
+nav = document.getElementsByClassName("nav")
+
 toggle.addEventListener('input', function(){
     if (toggle.checked)
     {
@@ -19,8 +21,10 @@ menutoggle.addEventListener('input', function(){
     if (menutoggle.checked)
     {
         menu.style.display = "inherit"
+        nav[0].style.display = "inherit"
     }
     else{
         menu.style.display= "none"
+        nav[0].style.display = "inherit"
     }
 })
